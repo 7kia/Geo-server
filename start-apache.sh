90686914a35fdbcfdcf99f553900f00ed82f3364
7kia <il7kia@yandex.ru>
2019-06-30 15:08:30 +0300

    Поправлены ссылки

--- start-apache.sh
+++ start-apache.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+﻿#!/bin/sh
 
 set -e
 
@@ -6,5 +6,5 @@ set -e
 
 mkdir /var/run/apache2
 mkdir /var/lock/apache2
-chown root:www-data /var/lock/apache2
+chown root:/var/www/geoserver /var/lock/apache2
 exec /usr/sbin/apache2 -k start -DFOREGROUND
